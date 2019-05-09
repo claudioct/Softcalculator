e05a40056b5ffe7c7e2eb69b58ef1e63db8aa3f6
ClaudioTorres <claudioct@gmail.com>
2019-05-09 15:52:25 -0300

    :truck: Changes folder structure
    Contains small refactoring in dockerfile too.

--- SoftCalculator.Api/Program.cs
+++ SoftCalculator.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace SoftCalculator
+namespace SoftCalculator.Api
 {
     public class Program
     {
